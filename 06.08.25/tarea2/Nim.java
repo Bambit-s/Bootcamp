f57be382f80e8b17615fd4950db646734452b24b
Vladimir Gromov <vovamakukha@gmail.com>
2025-08-07 08:55:18 -0300

    Nim update.

--- 06.08.25/tarea2/Nim.java
+++ 06.08.25/tarea2/Nim.java
@@ -4,88 +4,87 @@ import java.util.Random;
 class Player {
     private String name;
 
-    public Player() {
-        setname(name);
+    public Player(String name) {
+        this.name = name;
     }
 
-    public void setname(String name) {
-        Scanner scanner = new Scanner(System.in);
-        this.name = scanner.next();
-        // scanner.close(); // trouble
-    }
-
-    public String getname() {
+    public String getName() {
         return name;
     }
 
 }
 
 class Game {
-
+    private Scanner scaner;
     private String player1;
     private String player2;
+    private int[] field;
     private boolean start = true;
     Random nomber = new Random();
-    public int[] field = { nomber.nextInt(5) + 2, nomber.nextInt(5) + 2, nomber.nextInt(5) + 2 };
 
-    public Game(String player1, String player2) {
+    public Game(String player1, String player2, Scanner scaner) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.scaner = scaner;
+        field = new int[] { nomber.nextInt(5) + 2, nomber.nextInt(5) + 2, nomber.nextInt(5) + 2 };
 
-        setPlayer(player1, player2);
+        play();
+    }
+
+    private void play() {
         showNames();
         while (start) {
             showField();
-            playerchoose(player1);
-            if (summfield(field) <= 1) {
-                System.out.println(player2 + " lose...");
-                System.exit(0);
+            if (playerChoose(player1)) {
+                System.out.println(player1 + " lose...");
+                break;
             }
             showField();
-            playerchoose(player2);
-            if (summfield(field) <= 1) {
-                System.out.println(player1 + " lose...");
-                System.exit(0);
+            if (playerChoose(player2)) {
+                System.out.println(player2 + " lose...");
+                break;
             }
         }
-
     }
 
-    public void playerchoose(String player) {
-        System.out.print("\n" + player + ", choose letra:");
-        Scanner scannerLetra = new Scanner(System.in);
-        String chooseLetra = scannerLetra.nextLine();
+    public boolean playerChoose(String player) {
+        System.out.print("\n" + player + ", choose letra(A, B or C):");
+        int index = -1;
+        while (index == -1) {
+            String choice = scaner.nextLine().trim().toUpperCase();
+            if (choice.equals("A"))
+                index = 0;
+            else if (choice.equals("B"))
+                index = 1;
+            else if (choice.equals("C"))
+                index = 2;
+            else
+                System.out.println("Invalid letra!");
+        }
 
-        System.out.print("How much would you like to take from " + chooseLetra + ":");
-        Scanner scannerNumber = new Scanner(System.in);
-        int chooseNumber = scannerNumber.nextInt();
-
-        if (chooseLetra.equals("A")) {
-            field[0] = field[0] - chooseNumber;
+        System.out.print("How much would you like to take from " + (char) (index + 'A') + ":");
+        int number = -1;
+        while (number <= 0 || number > field[index]) {
+            try {
+                number = Integer.parseInt(scaner.nextLine());
+                if (number <= 0 || number > field[index]) {
+                    System.out.println("Invalid number. Try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Enter a number.");
+            }
         }
-        if (chooseLetra.equals("B")) {
-            field[1] = field[1] - chooseNumber;
-        }
-        if (chooseLetra.equals("C")) {
-            field[2] = field[2] - chooseNumber;
-        }
+        field[index] -= number;
+        return totalCounters() == 0;
     }
 
-    private int summfield(int[] field) {
-        int sum = 0;
-        for (int i = 0; i < field.length; i++) {
-            sum += field[i];
-        }
-        return sum;
+    private int totalCounters() {
+        return field[0] + field[1] + field[2];
     }
 
     public void showField() {
-        System.out.printf("A: " + field[0]);
-        System.out.printf("%5s%s", "", "B: " + field[1]);
-        System.out.printf("%5s%s", "", "C: " + field[2]);
-    }
-
-    public void setPlayer(String name1, String name2) {
-        this.player1 = name1;
-        this.player2 = name2;
+        System.out.println("\nCurrent field:");
+        System.out.println("A: " + field[0] + "\tB: " + field[1] + "\tC: " + field[2]);
     }
 
     public void showNames() {
@@ -98,13 +97,15 @@ class Game {
 public class Nim {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Put name of 1st Player");
-        Player player1 = new Player();
+        Player player1 = new Player(scanner.nextLine());
 
         System.out.println("Put name of 2st Player");
-        Player player2 = new Player();
+        Player player2 = new Player(scanner.nextLine());
 
-        Game game = new Game(player1.getname(), player2.getname());
+        Game game = new Game(player1.getName(), player2.getName(), scanner);
 
     }
 }
